eb03233aafb094e0f00c18f24b6ad1cd99b28001
JuanDNJ <juandevnjv@gmail.com>
2025-02-10 13:13:01 +0100

    fix: instalación de jsdom,@vitest/ui" y los tipos necesarios, configurando vitest y haciendo la primera prueba

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import App from './App'
+
+describe("App", () => {
+
+
+  it("App esta definido", () => {
+    expect(App).toBeDefined()
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: "./src/setupTest.ts",
+    outputFile: "json",
+    globals: true,
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      cleanOnRerun: true
+    },
+    css: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
+  }
 })
