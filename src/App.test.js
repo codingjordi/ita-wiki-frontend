a99004f7fd5c1071fe02fce00a1dddc383025d14
JuanDNJ <juandevnjv@gmail.com>
2025-02-10 12:39:13 +0100

    fix: Instalando React con vite

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/¡Bienvenid@ a la wiki de la IT Academy!/i);
-  expect(linkElement).toBeInTheDocument();
-});
