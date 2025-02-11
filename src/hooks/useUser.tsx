3c596a5c4ccf48f4aa6c7157d08134ab432f5931
JuanDNJ <juandevnjv@gmail.com>
2025-02-11 15:09:25 +0100

    refactor: Refactoring useUser hook

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
-export interface User {
-  uid: string;
-  displayName: string;
-  photoURL: string;
-}
+import { User } from "../types";
+import { signInWithGitHub } from "../api/firebase";
 
 export const useUser = () => {
-  const [user, setUser] = useState<User>();
-  return { user, setUser };
+  const [user, setUser] = useState<User | null>(JSON.parse(localStorage.getItem("user") as string));
+
+  const signIn = () => {
+    signInWithGitHub(setUser)
+  }
+  const signOut = () => {
+    localStorage.removeItem("user")
+    setUser(() => JSON.parse(localStorage.getItem("user") as string))
+  }
+  const saveUser = (user: User) => {
+    setUser(() => user)
+  }
+  return { user, saveUser, signIn, signOut };
 };
