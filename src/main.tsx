92f87b50c0add7339dd96597dc95f202f42f9f3c
JuanDNJ <juandevnjv@gmail.com>
2025-02-11 15:14:27 +0100

    fix: Using the new context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import UserCtxProvider from './context/UserCtxProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <UserCtxProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </UserCtxProvider>,
 )
