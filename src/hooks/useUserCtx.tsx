8b00df229b3198f9eb815826e1d237b708949e5b
JuanDNJ <juandevnjv@gmail.com>
2025-02-11 15:04:59 +0100

    fix: Editing hook, name change

--- src/hooks/useUserCtx.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { UserCtx } from "../context/User";
-
-export const useUserCtx = () => {
-  const ctx = useContext(UserCtx);
-  if (!ctx) throw new Error("contexto no definido");
-  return ctx;
-};
