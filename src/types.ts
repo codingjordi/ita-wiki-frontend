c9da41092f72bb7a498fa29d6ae4535ae7043c1a
JuanDNJ <juandevnjv@gmail.com>
2025-02-17 19:07:37 +0100

    fix: Añadiendo y actualizando los tipos

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,20 @@
-export interface User {
-  uid: string;
+import { ReactNode } from "react";
+
+export type TypChildren = {
+  children?: ReactNode
+}
+export interface IntUser {
+  id: string;
   displayName: string;
   photoURL: string;
+}
+
+export interface IntResource {
+  resource_id: number,
+  title: string,
+  description: string,
+  favorite: boolean,
+  votes: number,
+  user: IntUser,
+  create_at: string
 }
