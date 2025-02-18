fdd02796ad95e19d07204ca1d3ef3503083d7619
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2025-02-18 09:46:14 +0100

    fix: Modifying component names.

--- src/components/resources/__test__/BodyResource.test.tsx
+++ src/components/resources/__test__/BodyResource.test.tsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react"
 import { BodyResource } from "../BodyResource"
 
-test("The component must have the initial styles", () => {
+describe("BodyResource Component", () => {
 
-  render(
-    <BodyResource>
-      Content here
-    </BodyResource>
-  )
-
-  const bodyResource = screen.getByTestId("body-resource")
-  expect(bodyResource).toBeInTheDocument()
-  expect(bodyResource).toHaveClass("flex gap-2 w-full justify-between p-4")
+  it("The component must have the initial styles" , () => {
+    render(
+      <BodyResource>
+        Content here
+      </BodyResource>
+    )
+  
+    const bodyResource = screen.getByTestId("body-resource")
+    expect(bodyResource).toBeInTheDocument()
+    expect(bodyResource).toHaveClass("flex gap-2 w-full justify-between p-4")
+  })
 
 })
--- src/components/resources/__test__/VotesResource.test.tsx
+++ src/components/resources/__test__/VotesResource.test.tsx
@@ -2,11 +2,8 @@ import { render, screen } from "@testing-library/react"
 import { VotesResource } from "../VotesResource"
 const votes = 55 as number;
 
-describe('ResourceVotes Component', () => {
-  it("ResourceVotes esta definido", () => {
-    expect(VotesResource).toBeDefined()
-  })
-
+describe('VotesResource Component', () => {
+ 
   it("The component must have the initial styles", () => {
     render(<VotesResource votes={votes} />)
     const votesResource = screen.getByTestId("resource-votes")
