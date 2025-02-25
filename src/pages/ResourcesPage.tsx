47479d2720198bbf3bdb2adefde9beaa9eba05da
JuanDNJ <juandevnjv@gmail.com>
2025-02-25 11:10:38 +0100

    feture: Creando la página ResourcesPage

---
+++ src/pages/ResourcesPage.tsx
@@ -0,0 +1,27 @@
+import { FC, Fragment, useEffect, useState } from "react"
+import { useParams } from "react-router";
+
+import { IntResource, TypTechnologyResource } from "../types";
+import { ListResources } from "../components/resources/ListResources";
+import { getResources } from "../api/endPointResources";
+
+
+const ResourcesPage: FC = () => {
+  const { technology } = useParams()
+  const [apiResources, setApiResources] = useState<IntResource[]>([])
+  const tech = technology as TypTechnologyResource
+  useEffect(() => {
+    (async () => {
+      const data = await getResources()
+      setApiResources(() => data)
+    })()
+  }, [])
+
+  return (
+    <Fragment>
+      <ListResources resources={apiResources} technology={tech} />
+    </Fragment>
+  )
+}
+
+export default ResourcesPage;
