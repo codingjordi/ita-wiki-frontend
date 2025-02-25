c45cdb45522d4f0730908beaaf4dd5ea88dece8c
JuanDNJ <juandevnjv@gmail.com>
2025-02-25 21:30:50 +0100

    feature: Añadiendo svg heart y edit

--- src/components/resources/FavoriteResource.tsx
+++ src/components/resources/FavoriteResource.tsx
@@ -1,7 +1,19 @@
 import { FC } from "react"
+import heart from "../../assets/heart.svg"
+import edit from "../../assets/edit.svg"
 interface FavoriteResourceProps {
   favorite: boolean
 }
 export const FavoriteResource: FC<FavoriteResourceProps> = ({ favorite }) => {
-  return (<i role="resource" data-testid="favorite-resource" className="inline-flex">{favorite ? '💕' : '❤'}</i>)
+  return (
+    <div className="flex items-center justify-start gap-2 max-h-12">
+      <img src={edit} width={28} height={28} />
+      <i role="resource"
+        data-testid="favorite-resource"
+        className="inline-flex">{
+          favorite ? <img src={heart} width={28} height={28} /> : <img src={heart} width={28} height={28} />
+        }
+      </i>
+    </div>
+  )
 }
