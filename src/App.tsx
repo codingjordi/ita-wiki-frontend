e0da28111d744bb8221a5ab7852db34045454458
JuanDNJ <juandevnjv@gmail.com>
2025-02-25 11:02:24 +0100

    feature: Editando el App con los nuevos cambios del main

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { FC } from "react";
-import menu from "./assets/Vector-7.svg";
-
-import close from "./assets/close.svg";
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreateResourcePage from "./pages/CreateResourcePage";
-import moock from "./moock/resources.json"
-import avatarPost from "./assets/avatar_post.svg"
-import { ListResources } from "./components/resources/ListResources";
 import HeaderComponent from "./components/Layout/HeaderComponent";
 import AsideComponent from "./components/Layout/AsideComponent";
+import ResourcesPage from "./pages/ResourcesPage";
 
 const App: FC = () => {
 
@@ -20,21 +15,12 @@ const App: FC = () => {
         <AsideComponent />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/resources" element={<ListResources resources={moock.resources.map(res => {
-            return (
-              {
-                ...res,
-                user: {
-                  ...res.user,
-                  photoURL: avatarPost
-                }
-              }
-            )
-          })} nameResource="React.js" />} />
+          <Route path="/resources/:technology" element={<ResourcesPage />} />
           <Route path="/resource/add" element={<CreateResourcePage />} />
         </Routes>
       </div>
     </div>
+
   );
 };
 
