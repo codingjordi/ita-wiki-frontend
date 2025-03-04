1491c44796602f197d2bf349675218d1b411ac9c
codingjordi <jordisaperascg@gmail.com>
2025-03-04 21:30:28 +0100

    test: CreateResource.test.tsx created

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -15,7 +15,7 @@ const ResourcesPage: FC = () => {
     })();
   }, []);
 
-  return (<ListResogiurces resources={apiResources} technology={tech} />)
+  return (<ListResources resources={apiResources} technology={tech} />)
 }
 
 export default ResourcesPage;
