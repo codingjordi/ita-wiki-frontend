569d2a1d42005271c371047781a473b636fe21f1
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-06 16:14:56 +0100

    add: sonner toaster

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import UserCtxProvider from "./context/UserCtxProvider.tsx";
 import { BrowserRouter } from "react-router";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <UserCtxProvider>
     <StrictMode>
       <BrowserRouter>
         <App />
+        <Toaster />
       </BrowserRouter>
     </StrictMode>
   </UserCtxProvider>,
