7d484854828dbc656457713da2838c9e49049398
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-09 17:55:49 +0100

    refactor: componetization of CreateResourcePage.tsx

--- src/pages/CreateResourcePage.tsx
+++ src/pages/CreateResourcePage.tsx
@@ -2,97 +2,54 @@ import { IntResource } from "../types";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { resourceSchema } from "../validations/resourceSchema";
-import ButtonComponent from "../components/atoms/ButtonComponent";
+import FormInput from "../components/FormInput";
 import { createResource } from "../api/endPointResources";
 import { toast } from "sonner";
+import ButtonComponent from "../components/atoms/ButtonComponent";
 
 export default function CreateResourcePage() {
-
-  /* const { user } = useUser() */
-
-  const {register, handleSubmit, reset, formState: { errors }} = useForm<Partial<IntResource>>({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Partial<IntResource>>({
     resolver: zodResolver(resourceSchema),
   });
+
+  const onSubmit = async (data: Partial<IntResource>) => {
+    const resourceWithGithubId = {
+      ...data,
+      github_id: 4967235,
+    };
+
+    try {
+      await createResource(resourceWithGithubId);
+      toast.success("¡Recurso creado con éxito!");
+      reset();
+    } catch (error) {
+      console.error("Error al crear el recurso:", error);
+      toast.error("Hubo un error al crear el recurso");
+    }
+  };
 
   return (
     <div className="w-full">
       <h1 className="font-semibold">Nuevo recurso</h1>
       <div className="flex justify-center mt-20 xl:mr-[198px]">
         <form
-          onSubmit={handleSubmit(async (data: Partial<IntResource>) => {
-            const resourceWithGithubId = {
-              ...data,
-              github_id: 2421919,
-            };
-            try {
-              await createResource(resourceWithGithubId);
-              toast.success("Recurso creado con éxito!");
-              reset()
-            } catch (error) {
-              console.error("Error al crear el recurso:", error);
-              toast.error("Hubo un error al crear el recurso...");
-            }
-          })}
+          onSubmit={handleSubmit(onSubmit)}
           className="my-5 w-full lg:w-3/5 bg-white p-10 rounded-xl"
         >
-          <div>
-            <input
-              type="text"
-              id="title"
-              placeholder="Título"
-              className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879] "
-              {...register("title")}
-            />
-            <div className="h-6">
-              {errors.title && (
-                <p className="text-red-500 text-sm">{errors.title.message}</p>
-              )}
-            </div>
-          </div>
-  
-          <div>
-            <input
-              type="text"
-              id="description"
-              placeholder="Descripción"
-              className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879]"
-              {...register("description")}
-            />
-            <div className="h-6">
-              {errors.description && (
-                <p className="text-red-500 text-sm">{errors.description.message}</p>
-              )}
-            </div>
-          </div>
-  
-          <div>
-            <input
-              type="text"
-              id="url"
-              placeholder="URL"
-              className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879]"
-              {...register("url")}
-            />
-            <div className="h-6">
-              {errors.url && (
-                <p className="text-red-500 text-sm">{errors.url.message}</p>
-              )}
-            </div>
-          </div>
-  
-          <div>
-            <ButtonComponent
-              className="outline-[#B91879]"
-              children="Crear"
-              variant="primary"
-              type="submit"
-            />
-            <ButtonComponent
-              className="outline-[#B91879]"
-              children="Cancelar"
-              variant="secondary"
-              onClick={() => window.history.back()}
-              />
+          <FormInput id="title" placeholder="Título" register={register} errors={errors.title?.message} />
+
+          <FormInput id="description" placeholder="Descripción" register={register} errors={errors.description?.message} />
+
+          <FormInput id="url" placeholder="URL" register={register} errors={errors.url?.message} />
+
+          <div className="flex gap-4 mt-4">
+            <ButtonComponent type="submit" variant="primary">Crear</ButtonComponent>
+            <ButtonComponent variant="secondary" onClick={() => window.history.back()}>Cancelar</ButtonComponent>
           </div>
         </form>
       </div>
