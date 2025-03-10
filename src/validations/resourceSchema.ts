d31bbce967842954d78bfc5a579c7a0aa031e274
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-10 09:37:55 +0100

    add: validation on new fields 'category', 'theme' and 'type'

--- src/validations/resourceSchema.ts
+++ src/validations/resourceSchema.ts
@@ -1,15 +1,24 @@
 import { z } from "zod";
 import { IntResource } from "../types";
+import { themes } from "../data/themes";
+import { categories } from "../data/categories";
+import { resourceTypes } from "../data/resourceType";
 
 export const resourceSchema: z.ZodType<Partial<IntResource>> = z.object({
   title: z.string()
     .min(10, { message: "El título debe tener al menos 10 caracteres" })
     .max(50, { message: "El título debe tener menos de 50 caracteres" }),
+    
   description: z.string()
     .min(20, { message: "La descripción debe tener al menos 20 caracteres" })
     .max(150, { message: "La descripción debe tener menos de 150 caracteres" }),
+
   url: z.string()
     .url({ message: "Debe ser una URL válida. Indica el protocolo" })
     .max(300, { message: "La URL debe tener menos de 300 caracteres" }),
-  type: z.string().optional(),
+  category: z.enum(categories, { message: "Por favor, selecciona una categoría válida." }),
+
+  theme: z.enum(themes, { message: "Por favor, selecciona un tema válido" }),
+
+  type: z.enum(resourceTypes, { message: "Debes seleccionar al menos un tipo de recurso." })
 });
