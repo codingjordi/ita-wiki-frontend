7d7bbfcedab87c5cfa8394a412f5182474986506
Gordana Marjanovic <gogamar@gmail.com>
2025-03-10 13:21:12 +0100

    Run npx prettier --write .

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,24 +1,28 @@
 interface FormInputProps {
-    id: string;
-    placeholder: string;
-    register: any;
-    errors?: string; // Aquí errors ya es el mensaje del error del campo
-  }
-  
-  export default function FormInput({ id, placeholder, register, errors }: FormInputProps) {
-    return (
-      <div>
-        <input
-          type="text"
-          id={id}
-          placeholder={placeholder}
-          className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879]"
-          {...register(id)}
-        />
-        <div className="h-6">
-          {errors && <p className="text-red-500 text-sm">{errors}</p>}
-        </div>
+  id: string;
+  placeholder: string;
+  register: any;
+  errors?: string; // Aquí errors ya es el mensaje del error del campo
+}
+
+export default function FormInput({
+  id,
+  placeholder,
+  register,
+  errors,
+}: FormInputProps) {
+  return (
+    <div>
+      <input
+        type="text"
+        id={id}
+        placeholder={placeholder}
+        className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879]"
+        {...register(id)}
+      />
+      <div className="h-6">
+        {errors && <p className="text-red-500 text-sm">{errors}</p>}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/pages/CreateResourcePage.tsx
+++ src/pages/CreateResourcePage.tsx
@@ -44,24 +44,48 @@ export default function CreateResourcePage() {
           onSubmit={handleSubmit(onSubmit)}
           className="my-5 w-full lg:w-3/5 bg-white p-10 rounded-xl"
         >
-          <FormInput id="title" placeholder="Título" register={register} errors={errors.title?.message} />
-          <FormInput id="description" placeholder="Descripción" register={register} errors={errors.description?.message} />
-          <FormInput id="url" placeholder="URL" register={register} errors={errors.url?.message} />
+          <FormInput
+            id="title"
+            placeholder="Título"
+            register={register}
+            errors={errors.title?.message}
+          />
+          <FormInput
+            id="description"
+            placeholder="Descripción"
+            register={register}
+            errors={errors.description?.message}
+          />
+          <FormInput
+            id="url"
+            placeholder="URL"
+            register={register}
+            errors={errors.url?.message}
+          />
 
           <select
             id="category"
             className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879]"
             {...register("category")}
           >
-            <option value="Tecnología" className=" disabled:text-[#909AA3]" disabled selected>Tecnología</option>
-            {categories.map(categorie => (
+            <option
+              value="Tecnología"
+              className=" disabled:text-[#909AA3]"
+              disabled
+              selected
+            >
+              Tecnología
+            </option>
+            {categories.map((categorie) => (
               <option key={categorie} value={categorie}>
                 {categorie}
               </option>
             ))}
           </select>
           <div className="h-6">
-            {errors.category && <p className="text-red-500 text-sm">{errors.category.message}</p>}
+            {errors.category && (
+              <p className="text-red-500 text-sm">{errors.category.message}</p>
+            )}
           </div>
 
           <select
@@ -69,15 +93,24 @@ export default function CreateResourcePage() {
             className="w-full mb-1 px-6 py-4 border border-[#dddddd] rounded-lg placeholder:font-medium outline-[#B91879]"
             {...register("theme")}
           >
-            <option value="Tema" className=" disabled:text-[#909AA3]" disabled selected>Tema</option>
-            {themes.map(theme => (
+            <option
+              value="Tema"
+              className=" disabled:text-[#909AA3]"
+              disabled
+              selected
+            >
+              Tema
+            </option>
+            {themes.map((theme) => (
               <option key={theme} value={theme}>
                 {theme}
               </option>
             ))}
           </select>
           <div className="h-6">
-            {errors.theme && <p className="text-red-500 text-sm">{errors.theme.message}</p>}
+            {errors.theme && (
+              <p className="text-red-500 text-sm">{errors.theme.message}</p>
+            )}
           </div>
 
           <div className="flex justify-around">
@@ -113,17 +146,23 @@ export default function CreateResourcePage() {
             </div>
           </div>
 
-
-          {errors.type && <p className="text-red-500 text-sm">{errors.type.message}</p>}
+          {errors.type && (
+            <p className="text-red-500 text-sm">{errors.type.message}</p>
+          )}
 
           <div className="flex gap-4 mt-4">
-            <ButtonComponent type="submit" variant="primary">Crear</ButtonComponent>
-            <ButtonComponent variant="secondary" onClick={() => window.history.back()}>Cancelar</ButtonComponent>
+            <ButtonComponent type="submit" variant="primary">
+              Crear
+            </ButtonComponent>
+            <ButtonComponent
+              variant="secondary"
+              onClick={() => window.history.back()}
+            >
+              Cancelar
+            </ButtonComponent>
           </div>
         </form>
-        <pre>
-          {JSON.stringify(watch(), null, 2)}
-        </pre>
+        <pre>{JSON.stringify(watch(), null, 2)}</pre>
       </div>
     </div>
   );
