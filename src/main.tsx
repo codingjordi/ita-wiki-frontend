31583bb80a6758a7f1470e95aa9915b6f5c6d394
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-11 11:28:34 +0100

    add: using sonner toast to handle the outcome of a resource creation

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import "./index.css";
 import App from "./App.tsx";
 import UserCtxProvider from "./context/UserCtxProvider.tsx";
 import { BrowserRouter } from "react-router";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <UserCtxProvider>
     <StrictMode>
       <BrowserRouter>
+        <Toaster
+          richColors
+          toastOptions={{
+            style: {
+              padding: "2rem",
+              fontSize: "1rem",
+            },
+          }}
+        />
         <App />
       </BrowserRouter>
     </StrictMode>
