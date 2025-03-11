ebdb6290a41d5092173e63b48eb7b8ff336e67b1
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-11 11:29:54 +0100

    refactor: createResource funct. moved to endPointResources.ts

--- src/api/endPointResources.ts
+++ src/api/endPointResources.ts
@@ -34,4 +34,28 @@ const getResources = async (): Promise<IntResource[]> => {
   }
 };
 
-export { getResources };
+const createResource = async (resource: Partial<IntResource>) => {
+  try {
+    const response = await fetch(`${API_URL}${END_POINTS.resources.post}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(resource),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(
+        errorData.message || `Error ${response.status}: ${response.statusText}`,
+      );
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error al crear recurso:", error);
+    throw error;
+  }
+};
+
+export { getResources, createResource };
