b2ab327d7790752e6e8ad566ca55971d086ff8a2
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-16 20:09:53 +0100

    add: user's role added to UserContext

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,17 +1,22 @@
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getUserRole = async (githubId: number): Promise<string> => {
   try {
     const response = await fetch(
-      `${API_URL}/users/user-signedin-as?github_id=${githubId}`,
+      `${API_URL}login?github_id=${githubId}`,
     );
 
     //TO-DO: poner AbortController()
 
-    if (!response.ok) throw new Error("Error fetching user role");
+    if (!response.ok) return "anonymous" // provisional fix 
     const data = await response.json();
-    return data.role?.role || "anonymous";
-  } catch {
-    return "anonymous";
+    return data.role?.role;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message || "Error during fetching user role. Please, try again 👾");
+    }
+
+    throw new Error("Something went wrong, sis. Please, try again 👾");
   }
 };
