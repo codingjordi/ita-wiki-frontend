8574b47aa6852660ec4ad8aef9cb3029d01be0d2
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-17 21:31:39 +0100

    add: getUserRole(github_id) function created

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -4,12 +4,18 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const getUserRole = async (githubId: number): Promise<string> => {
   try {
     const response = await fetch(
-      `${API_URL}login?github_id=${githubId}`,
+      `${API_URL}login?github_id=${githubId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
     );
 
     //TO-DO: poner AbortController()
 
-    if (!response.ok) return "anonymous" // provisional fix 
+    if (!response.ok) return "anonymous" // provisional fix since the BE doesnt retourn 'anonymous'
+                                        // (BE currently return 'null' if user has no role in the DB)
     const data = await response.json();
     return data.role?.role;
   } catch (error: unknown) {
