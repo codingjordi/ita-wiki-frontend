2af0bc51d5693a8b1c7a71a24a0621dbe856ed50
Jordi Saperas Castro <jordisaperasc@gmail.com>
2025-03-18 13:19:07 +0100

    refactor: using POST + body instead of query params

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -2,17 +2,18 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export const getUserRole = async (githubId: number): Promise<string> => {
   try {
-    const response = await fetch(`${API_URL}login?github_id=${githubId}`, {
+    const response = await fetch(`${API_URL}login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify(githubId)
     });
 
     //TO-DO: poner AbortController()
 
     if (!response.ok) return "anonymous"; // provisional fix since the BE doesnt retourn 'anonymous'
-    // (BE currently return 'null' if user has no role in the DB)
+    // (B.E currently return 'null' if user has no role in the DB)
     const data = await response.json();
     return data.role?.role;
   } catch (error: unknown) {
@@ -22,7 +23,6 @@ export const getUserRole = async (githubId: number): Promise<string> => {
           "Error during fetching user role. Please, try again 👾",
       );
     }
-
     throw new Error("Something went wrong, sis. Please, try again 👾");
   }
 };
