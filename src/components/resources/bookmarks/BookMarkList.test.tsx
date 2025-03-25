82b710c5c8e8811337872ed2d2498cb33cc03299
Aleksei Chesnokov <173167461+Aleksei-Che@users.noreply.github.com>
2025-03-25 14:36:18 +0100

    Corregidos errores de tipos en los tests y actualizados los recursos mockeados

--- src/components/resources/bookmarks/BookMarkList.test.tsx
+++ src/components/resources/bookmarks/BookMarkList.test.tsx
@@ -27,6 +27,8 @@ describe("BookMarkList Component", () => {
       category: "Node",
       theme: "Todos",
       type: "Video",
+      votes: 10,
+      
     },
     {
       id: 2,
@@ -37,6 +39,7 @@ describe("BookMarkList Component", () => {
       category: "React",
       theme: "Todos",
       type: "Cursos",
+      votes: 20, 
     },
   ];
 
