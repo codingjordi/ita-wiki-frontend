14f748cfd02aac69f5b6b33eb9254273730b0c64
raquelEve <calculadoraverde@gmail.com>
2025-03-25 08:56:52 +0100

    fix prettier

--- src/components/Layout/header/SearchComponent.test.tsx
+++ src/components/Layout/header/SearchComponent.test.tsx
@@ -5,7 +5,7 @@ import SearchComponent from "./SearchComponent";
 describe("SearchComponent", () => {
   test("debe renderizar el input y el ícono", () => {
     render(
-      <SearchComponent onSearch={() => { }} disabled={false} resetTrigger="" />,
+      <SearchComponent onSearch={() => {}} disabled={false} resetTrigger="" />,
     );
 
     const input = screen.getByPlaceholderText(/buscar recurso/i);
@@ -65,7 +65,7 @@ describe("SearchComponent", () => {
 describe("SearchComponent disbled si le pasamos la prop", () => {
   it("debe deshabilitar el input cuando disabled es true", () => {
     render(
-      <SearchComponent onSearch={() => { }} disabled={true} resetTrigger="" />,
+      <SearchComponent onSearch={() => {}} disabled={true} resetTrigger="" />,
     );
 
     const input = screen.getByPlaceholderText(/buscar recurso/i);
