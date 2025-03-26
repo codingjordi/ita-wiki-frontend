11250fadea15931b95714eac73580c69df098bec
Orealba Soriano <orealbas@gmail.com>
2025-03-26 17:26:53 +0100

    fixed the modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
-import GitHubLogin from "../github-login/GitHubLogin";
-import { useCtxUser } from "../../hooks/useCtxUser";
+import GitHubLogin from '../github-login/GitHubLogin';
+import { useCtxUser } from '../../hooks/useCtxUser';
 
 interface ModalProps {
   closeModal: () => void;
@@ -12,11 +12,10 @@ export const Modal = ({ closeModal, title, children }: ModalProps) => {
   return (
     <>
       <div className="fixed inset-0 bg-black/30 flex justify-center items-center">
-        <div className="bg-white p-6 rounded-2xl w-[35%] h-[35%] relative">
+        <div className="bg-white p-6 rounded-2xl w-[35%] min-h-[30%] relative">
           <button
             className="absolute top-4 right-4 text-xl font-bold text-black"
-            onClick={closeModal}
-          >
+            onClick={closeModal}>
             X
           </button>
           <div className="flex flex-col justify-center items-center w-full mt-10">
