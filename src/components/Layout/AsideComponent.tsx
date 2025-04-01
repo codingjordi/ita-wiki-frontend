bd23cde1423f6802f1548b3af89fe07a6d6a1489
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-04-02 00:12:07 +0200

    changed resources list links design

--- src/components/Layout/AsideComponent.tsx
+++ src/components/Layout/AsideComponent.tsx
@@ -7,6 +7,7 @@ import php from "../../assets/logo-php 1.svg";
 import dataScience from "../../assets/data-science.svg";
 import bbdd from "../../assets/logo-bbdd 1.svg";
 import { Link, useLocation } from "react-router";
+import classNames from "classnames";
 
 const AsideComponent = () => {
   const asideContent = [
@@ -26,16 +27,26 @@ const AsideComponent = () => {
   return (
     <aside className="flex p-6 items-center justify-center lg:block lg:w-80 lg:h-screen lg:max-h-[calc(100vh-90px)] lg:mt-36">
       <ul className="space-y-6">
-        {asideContent.map((item, index) => (
-          <li key={index} className="flex items-center space-x-3">
-            <img src={item.icon} alt={item.label} className="w-6 h-6" />
-            <Link to={`/resources/${item.label}`}>
-              {currentPath === `/resources/${encodeURIComponent(item.label)}`
-                ? `• ${item.label}`
-                : item.label}
-            </Link>
-          </li>
-        ))}
+        {asideContent.map((item, index) => {
+          const path = `/resources/${item.label}`;
+          const isActive =
+            currentPath === `/resources/${encodeURIComponent(item.label)}`;
+
+          return (
+            <li key={index} className="flex items-center space-x-3">
+              <img src={item.icon} alt={item.label} className="w-6 h-6" />
+              <Link
+                to={path}
+                className={classNames("transition-colors", {
+                  "!text-[#b91879] !font-bold": isActive,
+                  "text-gray-700 ": !isActive,
+                })}
+              >
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
