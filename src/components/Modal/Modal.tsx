f976518dd3f28a296bb976d6bd4003226137492a
Orealba Soriano <orealbas@gmail.com>
2025-04-03 13:51:55 +0200

    changes in the modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
-import GitHubLogin from "../github-login/GitHubLogin";
-import { useCtxUser } from "../../hooks/useCtxUser";
+import GitHubLogin from '../github-login/GitHubLogin';
+import { useCtxUser } from '../../hooks/useCtxUser';
+import closeIcon from '../../../src/assets/close2.svg';
 
 interface ModalProps {
   closeModal: () => void;
@@ -9,18 +10,31 @@ interface ModalProps {
 export const Modal = ({ closeModal, title, children }: ModalProps) => {
   const { signIn } = useCtxUser();
 
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
   return (
     <>
-      <div className="fixed inset-0 bg-black/30 flex justify-center items-center z-50">
+      <div
+        className="fixed inset-0 bg-black/30 flex justify-center items-center z-50"
+        onClick={handleBackdropClick}>
         <div className="bg-white p-6 rounded-2xl w-[35%] min-h-[30%] relative">
           <button
-            className="absolute top-4 right-4 text-xl font-bold text-black"
-            onClick={closeModal}
-          >
-            X
+            className="absolute top-6 right-6 "
+            onClick={closeModal}>
+            <img
+              src={closeIcon}
+              alt="Cerrar modal"
+              className="w-[21px] h-[19px] "
+            />
           </button>
           <div className="flex flex-col justify-center items-center w-full mt-10">
-            {title && <h2 className="text-xl font-bold mb-10">{title}</h2>}
+            {title && (
+              <h2 className="text-[1.625rem] font-bold mb-10">{title}</h2>
+            )}
 
             {children ? children : <GitHubLogin onClick={signIn} />}
           </div>
