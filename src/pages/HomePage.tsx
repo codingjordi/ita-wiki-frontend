f281647f887442a083b516a7c234a4d24dbdecc9
Anna Hilla <annahilla21@gmail.com>
2025-04-04 17:09:12 +0200

    Remove unnecessary HTML tags

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,38 +21,34 @@ export default function HomePage() {
         <h1 className="font-bold text-3xl">
           ¡Bienvenid@ a la wiki de la IT Academy!
         </h1>
-        <section>
-          <div className="w-full flex flex-col items-center justify-center gap-10">
-            <div>
-              <ButtonComponent onClick={handleNavigate}>
-                Ver Recursos
-              </ButtonComponent>
-            </div>
-            <h2>Funcionalidades básicas que te ofrece esta plataforma:</h2>
-            <section className="flex flex-col gap-8 items-center md:items-stretch md:flex-row">
-              <Card
-                number={1}
-                imageSource={folder}
-                imageAlt="folder"
-                title="Guarda tus recursos favoritos"
-                text="Ten tus recursos bien organizados"
-              />
-              <Card
-                number={2}
-                imageSource={puzzle}
-                imageAlt="puzzle"
-                title="Colabora con tus compañer@s"
-                text="Recursos compartidos"
-              />
-              <Card
-                number={3}
-                imageSource={ok}
-                imageAlt="ok"
-                title="Vota los recursos"
-                text="La comunidad decide cuáles son más relevantes"
-              />
-            </section>
-          </div>
+        <div>
+          <ButtonComponent onClick={handleNavigate}>
+            Ver Recursos
+          </ButtonComponent>
+        </div>
+        <h2>Funcionalidades básicas que te ofrece esta plataforma:</h2>
+        <section className="flex flex-col gap-8 items-center md:items-stretch md:flex-row">
+          <Card
+            number={1}
+            imageSource={folder}
+            imageAlt="folder"
+            title="Guarda tus recursos favoritos"
+            text="Ten tus recursos bien organizados"
+          />
+          <Card
+            number={2}
+            imageSource={puzzle}
+            imageAlt="puzzle"
+            title="Colabora con tus compañer@s"
+            text="Recursos compartidos"
+          />
+          <Card
+            number={3}
+            imageSource={ok}
+            imageAlt="ok"
+            title="Vota los recursos"
+            text="La comunidad decide cuáles son más relevantes"
+          />
         </section>
       </main>
     </>
