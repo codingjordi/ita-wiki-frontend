e5ebe51446243c174dced7196fe28c99b5f449a1
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-04-08 13:25:14 +0200

    fixed label error

--- src/components/Layout/AsideComponent.tsx
+++ src/components/Layout/AsideComponent.tsx
@@ -56,7 +56,7 @@ const AsideComponent: React.FC<AsideComponentProps> = ({ asideContent }) => {
         <p className="space-y-3 py-6 font-bold text-lg">Categorias</p>
         <ul className="space-y-6">
           {asideContent.map((item, index) => {
-            const path = `/resources/${item.label}`;
+            const path = `/resources/${encodeURIComponent(item.label)}`;
             const isActive = isPathActive(path);
             const IconComponent = item.icon as unknown as React.FC<
               React.SVGProps<SVGSVGElement>
@@ -96,7 +96,7 @@ const AsideComponent: React.FC<AsideComponentProps> = ({ asideContent }) => {
                 to="/resources/bookmarks"
                 className={classNames("transition-colors", {
                   "!text-[var(--color-primary)] !font-bold": isPathActive(
-                    "/resources/bookmarks",
+                    "/resources/bookmarks"
                   ),
                   "text-gray-700": !isPathActive("/resources/bookmarks"),
                 })}
@@ -111,7 +111,7 @@ const AsideComponent: React.FC<AsideComponentProps> = ({ asideContent }) => {
                 to="/resources/my-resources"
                 className={classNames("transition-colors", {
                   "!text-[var(--color-primary)] !font-bold": isPathActive(
-                    "/resources/my-resources",
+                    "/resources/my-resources"
                   ),
                   "text-gray-700": !isPathActive("/resources/my-resources"),
                 })}
