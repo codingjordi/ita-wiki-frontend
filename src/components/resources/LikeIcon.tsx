2f5b34b65eaa7df8189ed70d7b41dc0f3044aba1
Varos1009 <varos1009@gmail.com>
2025-04-27 14:57:24 +0200

    style: fix LikeIcon formatting

--- src/components/resources/LikeIcon.tsx
+++ src/components/resources/LikeIcon.tsx
@@ -2,28 +2,23 @@ import { FC } from "react";
 import { Triangle } from "lucide-react";
 
 interface LikeIconProps {
-    liked?: boolean;
+  liked?: boolean;
 }
 
-const LikeIcon: FC<LikeIconProps> = ({
-    liked = false }) => {
-    return (
-        <div
-            id="likeIcon"
-            data-testid="likeIcon"
-            className="flex items-center justify-start gap-2 max-h-12"
-        >
-            <Triangle
-                size={16}
-                fill={liked ? "green" : "none"}
-                color={liked ? "green" : "gray"}
-                aria-label={
-                    liked
-                        ? "Me gusta"
-                        : ""
-                }
-            />
-        </div>
-    );
-}
+const LikeIcon: FC<LikeIconProps> = ({ liked = false }) => {
+  return (
+    <div
+      id="likeIcon"
+      data-testid="likeIcon"
+      className="flex items-center justify-start gap-2 max-h-12"
+    >
+      <Triangle
+        size={16}
+        fill={liked ? "green" : "none"}
+        color={liked ? "green" : "gray"}
+        aria-label={liked ? "Me gusta" : ""}
+      />
+    </div>
+  );
+};
 export default LikeIcon;
