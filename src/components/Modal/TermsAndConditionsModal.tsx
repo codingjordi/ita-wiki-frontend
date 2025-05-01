70d8e80f6a2032d7571872724ad4f387207f680d
LaraBerenguer <laraberenguer@outlook.com>
2025-05-01 18:14:58 +0200

    Delete close button to mirror figma design

--- src/components/Modal/TermsAndConditionsModal.tsx
+++ src/components/Modal/TermsAndConditionsModal.tsx
@@ -1,4 +1,3 @@
-import closeIcon from "../../../src/assets/close2.svg";
 import TermsAndConditionsComponent from "../Layout/header/TermsAndConditionsComponent";
 
 interface TermsAndConditionsModalProps {
@@ -19,14 +18,7 @@ export const TermsAndConditionsModal = ({ closeModal, title }: TermsAndCondition
                 className="fixed inset-0 bg-black/30 flex justify-center items-center z-50"
                 onClick={handleBackdropClick}
             >
-                <div className="bg-white px-28 py-6 rounded-2xl w-[70%] min-h-[35%] relative">
-                    <button className="absolute top-6 right-6 " onClick={closeModal}>
-                        <img
-                            src={closeIcon}
-                            alt="Cerrar modal"
-                            className="w-[21px] h-[19px] "
-                        />
-                    </button>
+                <div className="bg-white px-28 py-6 rounded-2xl w-[70%] min-h-[35%] relative">                    
                     <div className="flex flex-col justify-center items-start mt-10">
                         {title && (
                             <h2 className="text-[1.7rem] text-black font-extrabold mb-10">
