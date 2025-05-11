6ad5202038b6148f11f0f16a69740243320917b4
LaraBerenguer <laraberenguer@outlook.com>
2025-05-11 16:13:14 +0200

    Add custom hook for changing user roles

---
+++ src/hooks/useChangeUserRole.ts
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useCtxUser } from "./useCtxUser";
+import { changeRole } from "../api/endPointChangeRole";
+import { toast } from "sonner";
+
+
+export function useChangeUserRole() {
+    const { user, saveUser } = useCtxUser();
+    const [isChanging, setIsChanging] = useState<boolean>(false);
+
+    const updateUserRole = async (newRole: string) => {
+        if (!user || !user.id) return false;
+        setIsChanging(true);
+
+        try {
+            const request = {
+                github_id: user.id,
+                role: newRole
+            };
+
+            const response = await changeRole(request);
+
+            if (response && response.role) {
+                saveUser({ ...user, role: newRole });
+                toast.success(`Rol cambiado a: ${newRole}`);
+                return true;
+            };
+            return false;
+        } catch (error) {
+            console.error("Error changing role:", error);
+            toast.error("No se pudo cambiar el rol");
+            return false;
+        } finally {
+            setIsChanging(false);
+        }
+    };
+
+    return {
+        updateUserRole,
+        isChanging
+    };
+};
