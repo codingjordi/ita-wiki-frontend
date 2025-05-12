6345fb68415ac10bc3019111bc76b1d6bc1344f4
LaraBerenguer <laraberenguer@outlook.com>
2025-05-12 13:16:28 +0200

    Add temporary roles constant.

---
+++ src/data/tempRoles.ts
@@ -0,0 +1,9 @@
+//This is a temporal solution until backend endpoint GET roles.
+
+export const roles = {
+    SUPERADMIN: 'superadmin',
+    ADMIN: 'admin',
+    MENTOR: 'mentor',
+    STUDENT: 'student',
+    ANONYMOUS: 'anonymous'
+} as const;
