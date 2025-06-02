8d9c83b23e08c7d6ea6b41423005fe5926d9ded2
JordiLara <jordilarareyes@gmail.com>
2025-06-02 14:08:43 +0200

    adds the conditional to LikeIcon from the ResourceCard to change color

--- src/components/resources/LikeIcon.tsx
+++ src/components/resources/LikeIcon.tsx
@@ -9,8 +9,7 @@ const LikeIcon: FC<LikeIconProps> = ({ liked = false }) => {
   return (
     <Triangle
       size={16}
-      fill={liked ? "rgb(39, 174, 96)" : "none"}
-      color={liked ? "rgb(39, 174, 96)" : "black"}
+      className={`${liked ? "text-green-custom fill-green-custom" : "text-black"}`}
       aria-label={liked ? "Me gusta" : ""}
     />
   );
