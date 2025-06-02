e6aa7345ba5880709a097c71728331d905ed1ced
JordiLara <jordilarareyes@gmail.com>
2025-06-02 16:31:58 +0200

    changes the shape of the border around the voting button

--- src/components/ui/ResourceCard.tsx
+++ src/components/ui/ResourceCard.tsx
@@ -102,7 +102,7 @@ const ResourceCard: FC<ResourceCardProps> = ({
         </div>
         <div
           onClick={() => !disabled && handleLike()}
-          className={`flex flex-col items-center justify-center border border-gray-200 rounded-lg px-3 py-2 ${
+          className={`flex flex-col items-center justify-center border border-gray-200 rounded-lg px-4 py-2 ${
             disabled ? "opacity-70 cursor-not-allowed" : "cursor-pointer"
           }`}
         >
