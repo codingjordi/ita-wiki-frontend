37cd9da5f87fdbec5ed61ef804c563444c183c87
francamateus93 <francamateus@gmail.com>
2025-06-02 10:46:54 +0200

    Fix failure in getUserRole to expect arguments

--- src/config.test.ts
+++ src/config.test.ts
@@ -1,19 +1,28 @@
-import { API_URL, END_POINTS } from "./config";
+// import { API_URL, END_POINTS } from "./config";
 
 describe("Configuración de la API", () => {
   // Simulando la variable de entorno antes de cada test
-  beforeAll(() => {
-    // Mock de la variable de entorno para que VITE_API_URL tenga un valor conocido durante el test
-    import.meta.env.VITE_API_URL = "http://localhost:8000/api/";
+  beforeEach(() => {
+    vi.resetModules();
+    Object.defineProperty(import.meta, "env", {
+      value: {
+        ...import.meta.env,
+        VITE_API_URL: "http://localhost:8000/api/",
+      },
+      writable: false,
+      configurable: true,
+    });
   });
 
-  it("debería tener la URL correcta de la API", () => {
+  it("debería tener la URL correcta de la API", async () => {
     // Verificar que la variable API_URL tenga el valor correcto
-    expect(API_URL).toBe("http://localhost:8000/api/");
+    const { API_URL } = await import("./config");
+    expect(API_URL).toBe("http://localhost:8000");
   });
 
-  it("debería tener las rutas correctas en END_POINTS", () => {
+  it("debería tener las rutas correctas en END_POINTS", async () => {
     // Verificar que los endpoints en END_POINTS sean los correctos
+    const { END_POINTS } = await import("./config");
     expect(END_POINTS.resources.lists).toBe("resources/");
   });
 });
