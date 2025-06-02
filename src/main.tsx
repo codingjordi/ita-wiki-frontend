5f04d47604d968aec1984a7878a343a9f3418577
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-06-02 13:02:47 +0200

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ import { LikesProvider } from "./context/LikeContext.tsx";
 import { TagsProvider } from "./context/TagsContext.tsx";
 import { UserProvider } from "./context/UserContext";
 
-
 createRoot(document.getElementById("root")!).render(
   <UserProvider>
     <StrictMode>
