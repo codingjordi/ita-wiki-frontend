b170592dbd6cb6afc5bcf3f1d42c215943c05669
LuisAlejandroCortesGalan <wincho_04@hotmail.com>
2025-06-09 11:34:38 +0200

    style: fix prettier formatting

--- src/components/resources/FilterResources.tsx
+++ src/components/resources/FilterResources.tsx
@@ -53,7 +53,7 @@ export const FilterResources: FC<FilterResourcesProps> = ({
       setSelectedTags(
         selectedTags.includes(tag)
           ? selectedTags.filter((t) => t !== tag)
-          : [...selectedTags.filter((t) => t !== "Todos"), tag]
+          : [...selectedTags.filter((t) => t !== "Todos"), tag],
       );
     }
   };
