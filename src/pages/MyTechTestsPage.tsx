36f8377204e893dbdcd04be2c622a5da67d47855
OhMyLaia <ohmylaia@live.com>
2025-07-08 19:51:39 +0200

    component shows mock json titles

---
+++ src/pages/MyTechTestsPage.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import '../../public/technical-tests-mock.json';
+import { useLocation } from "react-router";
+
+function MyTechTestsPage() {
+
+    const location = useLocation();
+
+
+    const [ techTests, setTechTests] = useState<any[]>([]);
+
+    // const getTechTestList = async () => {
+
+    //     try {
+    //         const response = await fetch(`${import.meta.env.VITE_API_URL}/technicaltests`);
+
+    //         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+    //         const data = await response.json();
+
+    //         console.log(data);
+    //         return data;
+            
+    //     } catch (err) {
+    //         console.error('Error:', err);
+    //     }
+    // }
+
+    const getTechTestListMock = async () => {
+
+        try {
+
+            const response = await fetch('/technical-tests-mock.json');
+
+            if (!response) throw new Error(`could not fetch tech-tests mock data`)
+            
+            const data = await response.json();
+            console.log(data);
+
+            return data;
+
+        } catch (err) {
+            console.error('Error:', err);
+        }
+    }
+
+    useEffect(() => {
+        getTechTestListMock().then((data) => {
+        setTechTests((prev) => [...prev, ...data.data]);
+        });
+    }, [location.key]);
+
+
+
+    return (
+        <div className="
+        flex
+        flex-col
+        m-4 p-4
+        w-fit bg-gray-50
+        rounded shadow-xl
+        ">
+            <ul className="">
+            {techTests.map((test: object) => (
+                <li key={test.id}>
+                    - {test.title}
+                    </li>
+            ))}
+            </ul>
+        </div>
+    )
+}
+
+export default MyTechTestsPage;
