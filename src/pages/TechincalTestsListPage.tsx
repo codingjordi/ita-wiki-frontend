14240f8a0fddaf615eff92c632d61cebd04a4911
OhMyLaia <ohmylaia@live.com>
2025-07-08 19:01:14 +0200

    added mock tech tests json to fetch

--- src/pages/TechincalTestsListPage.tsx
+++ src/pages/TechincalTestsListPage.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState } from "react";
 
 function TechincalTestsListPage() {
+
+
+    const [ techTests, setTechTests] = useState([]);
+
+    const getTechTestList = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/technicaltests`);
+
+            if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+            const data = await response.json();
+            console.log(data);
+
+        } catch (err) {
+
+        }
+    }
+
     return (
         <div>
-            
+
         </div>
     )
 }
