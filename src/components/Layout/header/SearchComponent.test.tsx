3306393c74d38bea0f2d8ad5f706f7fb800e7862
Mika7596 <mika7596@gmail.com>
2025-07-10 00:02:29 +0200

    Updated test file for search bar

--- src/components/Layout/header/SearchComponent.test.tsx
+++ src/components/Layout/header/SearchComponent.test.tsx
@@ -3,10 +3,8 @@ import { describe, it, expect, vi } from "vitest";
 import SearchComponent from "./SearchComponent";
 
 describe("SearchComponent", () => {
-  test("debe renderizar el input y el ícono", () => {
-    render(
-      <SearchComponent onSearch={() => {}} disabled={false} resetTrigger="" />,
-    );
+  it("debe renderizar el input y el ícono", () => {
+    render(<SearchComponent onSearch={() => {}} resetTrigger="" />);
 
     const input = screen.getByPlaceholderText(/buscar recurso/i);
     expect(input).toBeInTheDocument();
@@ -14,61 +12,30 @@ describe("SearchComponent", () => {
     const icon = screen.getByAltText(/buscar/i);
     expect(icon).toBeInTheDocument();
   });
-});
 
-describe("SearchComponent", () => {
   it("debe actualizar el valor del input al escribir y llamar a onSearch", () => {
     const mockOnSearch = vi.fn();
-    render(
-      <SearchComponent
-        onSearch={mockOnSearch}
-        disabled={false}
-        resetTrigger=""
-      />,
-    );
+    render(<SearchComponent onSearch={mockOnSearch} resetTrigger="" />);
 
     const input = screen.getByPlaceholderText(/buscar recurso/i);
     fireEvent.change(input, { target: { value: "React" } });
 
     expect(input).toHaveValue("React");
-
     expect(mockOnSearch).toHaveBeenCalledWith("React");
   });
-});
 
-describe("SearchComponent", () => {
   it("debe resetear el input cuando cambia resetTrigger", () => {
     const mockOnSearch = vi.fn();
     const { rerender } = render(
-      <SearchComponent
-        onSearch={mockOnSearch}
-        disabled={false}
-        resetTrigger="initial"
-      />,
+      <SearchComponent onSearch={mockOnSearch} resetTrigger="initial" />,
     );
 
     const input = screen.getByPlaceholderText(/buscar recurso/i);
     fireEvent.change(input, { target: { value: "React" } });
     expect(input).toHaveValue("React");
 
-    rerender(
-      <SearchComponent
-        onSearch={mockOnSearch}
-        disabled={false}
-        resetTrigger="new"
-      />,
-    );
+    rerender(<SearchComponent onSearch={mockOnSearch} resetTrigger="new" />);
 
     expect(input).toHaveValue("");
-  });
-});
-describe("SearchComponent disbled si le pasamos la prop", () => {
-  it("debe deshabilitar el input cuando disabled es true", () => {
-    render(
-      <SearchComponent onSearch={() => {}} disabled={true} resetTrigger="" />,
-    );
-
-    const input = screen.getByPlaceholderText(/buscar recurso/i);
-    expect(input).toBeDisabled();
   });
 });
