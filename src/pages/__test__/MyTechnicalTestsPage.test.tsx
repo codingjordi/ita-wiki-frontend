44c739461a140039bf3f77f0fb45abeb8a39253c
DaniloEspinosa <115655795+DaniloEspinosa@users.noreply.github.com>
2025-07-18 16:08:12 +0200

    feat: add new technical test components and pages

---
+++ src/pages/__test__/MyTechnicalTestsPage.test.tsx
@@ -0,0 +1,83 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import MyTechnicalTestsPage from "../MyTechnicalTestsPage";
+import { vi } from "vitest";
+import { MemoryRouter } from "react-router";
+
+describe("MyTechnicalTestsPage", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("renders without crashing", () => {
+    render(
+      <MemoryRouter>
+        <MyTechnicalTestsPage />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText("Cargando tests técnicos...")).toBeInTheDocument();
+  });
+
+  it("fetches and displays tech test titles from mock data", async () => {
+    const mockData = {
+      data: [
+        {
+          id: 1,
+          title: "Test A",
+          language: "JavaScript",
+          description: "Test description A",
+          tags: ["tag1"],
+        },
+        {
+          id: 2,
+          title: "Test B",
+          language: "TypeScript",
+          description: "Test description B",
+          tags: ["tag2"],
+        },
+      ],
+    };
+
+    global.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve(mockData),
+      }),
+    ) as unknown as typeof fetch;
+
+    render(
+      <MemoryRouter>
+        <MyTechnicalTestsPage />
+      </MemoryRouter>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText("- Test A")).toBeInTheDocument();
+      expect(screen.getByText("- Test B")).toBeInTheDocument();
+    });
+  });
+
+  it("handles empty data", async () => {
+    const emptyMockData = { data: [] };
+
+    global.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve(emptyMockData),
+      }),
+    ) as unknown as typeof fetch;
+
+    render(
+      <MemoryRouter>
+        <MyTechnicalTestsPage />
+      </MemoryRouter>,
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText("- Test A")).not.toBeInTheDocument();
+    });
+  });
+});
