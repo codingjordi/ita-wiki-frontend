34f505dbfc94f4032b771a5bd6217b278d36d8e6
OhMyLaia <ohmylaia@live.com>
2025-07-24 11:18:25 +0200

    fix: refactored error handling logic in technical tests page in order to pass the tests

--- src/pages/MyTechnicalTestsPage.tsx
+++ src/pages/MyTechnicalTestsPage.tsx
@@ -5,23 +5,22 @@ function MyTechnicalTestsPage() {
   const { technicalTests, isLoading, error } = useTechnicalTests();
   const navigate = useNavigate();
 
-  if (isLoading) {
-    return <p className="m-4">Cargando pruebas técnicas...</p>;
-  }
-
-  if (error) {
-    return <p className="m-4 text-red-500">Error: {error.message}</p>;
-  }
-
   return (
     <div>
-      <div className="flex flex-col m-4 p-4 w-fit bg-gray-50 rounded shadow-xl">
-        <ul>
-          {technicalTests.map((test) => (
-            <li key={test.id}>- {test.title}</li>
-          ))}
-        </ul>
-      </div>
+      {isLoading ? (
+        <p className="m-4">Cargando pruebas técnicas...</p>
+      ) : error ? (
+        <p className="m-4 text-red-500">Error: {error.message}</p>
+      ) : (
+        <div className="flex flex-col m-4 p-4 w-fit bg-gray-50 rounded shadow-xl">
+          <ul>
+            {technicalTests.map((test) => (
+              <li key={test.id}>- {test.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+  
       <div className="bg-white w-fit p-2 m-4 rounded hover:border-2 hover:border-black">
         <button onClick={() => navigate("/resources/technical-test/create")}>
           Crear prueba
