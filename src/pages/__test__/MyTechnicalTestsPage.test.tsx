bd8517469939ad9a03f2afc5db8e90836994eab5
OhMyLaia <ohmylaia@live.com>
2025-07-24 11:07:23 +0200

    fix: fixed tests

--- src/pages/__test__/MyTechnicalTestsPage.test.tsx
+++ src/pages/__test__/MyTechnicalTestsPage.test.tsx
@@ -15,6 +15,22 @@ vi.mock("react-router", async () => {
   };
 });
 
+vi.mock("../hooks/useTechnicalTests", () => ({
+  default: () => ({
+    technicalTests: [
+      {
+        id: 1,
+        title: "Test A",
+        language: "JavaScript",
+        description: "Test description A",
+        tags: ["tag1"],
+      },
+    ],
+    isLoading: false,
+    error: null,
+  }),
+}));
+
 describe("MyTechnicalTestsPage", () => {
   beforeEach(() => {
     vi.clearAllMocks();
@@ -100,9 +116,10 @@ describe("MyTechnicalTestsPage", () => {
       </MemoryRouter>,
     );
 
-    const button = screen.getByRole("button", { name: /Crear prueba/i });
+    const button = await screen.findByRole("button", { name: /Crear prueba/i });
     expect(button).toBeInTheDocument();
 
+    button.click();
     expect(mockedNavigate).toHaveBeenCalledWith(
       "/resources/technical-test/create",
     );
