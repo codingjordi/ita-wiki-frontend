fd4fc8428d0712b6d23ba4e8c78727382cfc90b6
Mika7596 <mika7596@gmail.com>
2025-07-28 11:04:49 +0200

    Modified languages list so that it matches with enum language in backend

--- src/components/Layout/aside/asideContent.ts
+++ src/components/Layout/aside/asideContent.ts
@@ -3,15 +3,13 @@ import { FC, SVGProps } from "react";
 type SvgIcon = FC<SVGProps<SVGSVGElement>>;
 
 import CircleIcon from "@/assets/circle-icon.svg?react";
-import { Category } from "../../../types";
 
-export const asideContent: { icon: SvgIcon; label: Category }[] = [
-  { icon: CircleIcon, label: "Node" },
-  { icon: CircleIcon, label: "React" },
-  { icon: CircleIcon, label: "Angular" },
+export const asideContent: { icon: SvgIcon; label: string }[] = [
+  { icon: CircleIcon, label: "PHP" },
   { icon: CircleIcon, label: "JavaScript" },
   { icon: CircleIcon, label: "Java" },
-  { icon: CircleIcon, label: "Fullstack PHP" },
-  { icon: CircleIcon, label: "Data Science" },
-  { icon: CircleIcon, label: "BBDD" },
+  { icon: CircleIcon, label: "React" },
+  { icon: CircleIcon, label: "TypeScript" },
+  { icon: CircleIcon, label: "Python" },
+  { icon: CircleIcon, label: "SQL" },
 ];
