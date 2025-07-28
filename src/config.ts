25b92e63647098bf55c41667c519b7daaa6798b6
Mika7596 <mika7596@gmail.com>
2025-07-28 14:00:47 +0200

    Changed endpoint in config.ts

--- src/config.ts
+++ src/config.ts
@@ -43,8 +43,8 @@ const END_POINTS = {
     roleChange: "feature-flags/role-self-assignment" as EndPoints,
   },
   technicaltests: {
-    create: "technical-tests/" as EndPoints,
-    get: "technical-tests/" as EndPoints,
+    create: "technical-tests" as EndPoints,
+    get: "technical-tests" as EndPoints,
   },
 };
 
